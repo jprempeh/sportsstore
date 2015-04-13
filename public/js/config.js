93a6024bc68796f423751e482e0e824ef89beec6
Joe Prempeh <prempehjoe@gmail.com>
2015-04-13 12:44:45 -0700

    Initial commit

---
+++ public/js/config.js
@@ -0,0 +1,37 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
+
+    // Configure Idle settings
+    IdleProvider.idle(5); // in seconds
+    IdleProvider.timeout(120); // in seconds
+
+    $urlRouterProvider.otherwise("/create");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('create', {
+            abstract: true,
+            url: "/create",
+            templateUrl: "views/create.html"
+        })
+
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
